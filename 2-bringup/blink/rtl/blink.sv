1b548bc9f9b166c837160a4bb20652da76b02534
OrangeAFK <onepluslion@gmail.com>
2026-09-25 18:19:26 -0500

    added blink rtl

--- 2-bringup/blink/rtl/blink.sv
+++ 2-bringup/blink/rtl/blink.sv
@@ -1,25 +1,23 @@
-// Simple free-running LED blinker.
-// LED toggles every TOGGLE_CYCLES clocks after reset deasserts.
 module blink #(
-  parameter int TOGGLE_CYCLES = 10
-) (
-  input  logic clk,
-  input  logic rst_n,
+  parameter int TOGGLE_CYCLES = 10;
+)(
+  input logic clk,
+  input logic rst_n,
   output logic led
 );
 
-  logic [$clog2(TOGGLE_CYCLES)-1:0] cnt;
+logic [$clog2(TOGGLE_CYCLES)-1:0] cnt;
 
-  always_ff @(posedge clk or negedge rst_n) begin
-    if (!rst_n) begin
-      cnt <= '0;
-      led <= 1'b0;
-    end else if (cnt == TOGGLE_CYCLES - 1) begin
-      cnt <= '0;
-      led <= ~led;
-    end else begin
-      cnt <= cnt + 1'b1;
-    end
+always_ff @(posedge clk or negedge rst_n) begin
+  if(!rst_n) begin
+    cnt <= '0;
+    led <= 1'b0;
+  end else if (cnt == TOGGLE_CYCLES - 1) begin
+    cnt <= '0;
+    led <= ~led;
+  end else begin
+    cnt <= cnt + 1'b1;
   end
+end
 
 endmodule
